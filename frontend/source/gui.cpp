156e81eb9e5d4d69ef1df367b403e842b4f4e1f3
noword <noword.forever@gmail.com>
2024-03-22 23:25:07 +0800

    update

--- frontend/source/gui.cpp
+++ frontend/source/gui.cpp
@@ -33,6 +33,34 @@ void Gui::Run()
 
     ImGui_ImplVita2D_NewFrame();
 
+    ImGuiTabBarFlags tab_bar_flags = ImGuiTabBarFlags_None;
+
+    ImGui::SetNextWindowPos({0, 0});
+    ImGui::SetNextWindowSize({960, 544});
+    ImGui::Begin("Hello",
+                 NULL,
+                 ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoDecoration);
+    if (ImGui::BeginTabBar("MyTabBar", tab_bar_flags))
+    {
+        if (ImGui::BeginTabItem("Avocado"))
+        {
+            ImGui::Text("This is the Avocado tab!\nblah blah blah blah blah");
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("Broccoli"))
+        {
+            ImGui::Text("This is the Broccoli tab!\nblah blah blah blah blah");
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("Cucumber"))
+        {
+            ImGui::Text("This is the Cucumber tab!\nblah blah blah blah blah");
+            ImGui::EndTabItem();
+        }
+        ImGui::EndTabBar();
+    }
+    ImGui::End();
+
     ImGui::Render();
     ImGui_ImplVita2D_RenderDrawData(ImGui::GetDrawData());
 }
