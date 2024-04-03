d42e962495d54b99e89892c2a60bbffad2944f2e
noword <noword@sina.com>
2024-04-03 17:16:04 +0800

    1

--- frontend/source/config.cpp
+++ frontend/source/config.cpp
@@ -6,7 +6,12 @@
 Config::Config()
 {
     key_maps = {
-        {RETRO_JOYPAD_B, SCE_CTRL_UP},
+        {RETRO_JOYPAD_UP, SCE_CTRL_UP},
+        {RETRO_JOYPAD_RIGHT, SCE_CTRL_RIGHT},
+        {RETRO_JOYPAD_DOWN, SCE_CTRL_DOWN},
+        {RETRO_JOYPAD_LEFT, SCE_CTRL_LEFT},
+        {RETRO_JOYPAD_START, SCE_CTRL_START},
+        {RETRO_JOYPAD_SELECT, SCE_CTRL_SELECT},
     };
 }
 
