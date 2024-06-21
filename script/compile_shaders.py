f2de9d611539d91a66bf54504a363a5c4f853b8f
noword <noword.forever@gmail.com>
2024-06-21 16:55:15 +0800

    1

---
+++ script/compile_shaders.py
@@ -0,0 +1,8 @@
+import os
+from pathlib import Path
+
+for name in Path('shaders').rglob('*.cg'):
+    print(name)
+    stem = name.stem
+    os.system(f'psp2cgc.exe -profile sce_vp_psp2 -DVERTEX {name} -o {stem}_v.gxp')
+    os.system(f'psp2cgc.exe -profile sce_fp_psp2 -DFRAGMENT {name} -o {stem}_f.gxp')
